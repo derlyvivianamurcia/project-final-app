716a540df94c7210c49aead9d78639e16682502d
yeraldith <yeraldith.camargo@gmail.com>
2020-04-11 14:44:10 -0500

    creación de sección productos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="container-fluid bg-light">
             <nav className="d-flex justify-content-center">
                 <ul className="navbar sticky-top navbar-light">
-                    <a className="nav-link" href="#"> <img src="https://img.icons8.com/nolan/64/twitter.png" /></a>
+                    <a className="nav-link anime" href="#"> <img src="https://img.icons8.com/nolan/64/twitter.png" /></a>
                     <a className="nav-link" href="#">  <img src="https://img.icons8.com/nolan/64/instagram-new.png" /></a>
                     <a className="nav-link" href="#">  <img src="https://img.icons8.com/nolan/64/linkedin.png" /></a>
                 </ul>
