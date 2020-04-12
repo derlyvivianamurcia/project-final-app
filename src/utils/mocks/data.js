2dc021aa797758e09b248d99aa479209c9bd9a92
Derly <derlyvivianamurcia@gmail.com>
2020-04-12 13:45:09 -0500

    Úpdate empleos

--- src/utils/mocks/data.js
+++ src/utils/mocks/data.js
@@ -28,6 +28,34 @@ export const data = {
       ubicacionEmpleo: "London, GB",
       descripcionEmpleo: 'Who we are Yoti is a global company whose aim is to be the world rsquo s trusted identity system We have seriously ambitious goals to become the world s most secure and data sensitive company Our passionate team works to improve the way people share their personal information Innovative',
       tipoEmpleo: 'Jornada completa'
+    },
+    {
+      imgEmpleo: 'https://media-exp1.licdn.com/dms/image/C5103AQEIZ795G85ZYQ/profile-displayphoto-shrink_100_100/0?e=1592438400&v=beta&t=4JpNRXVWTcjJw5ptawYBJ4YLZooarHHnoRECp7tZIFs',
+      nameEmpleo: "Junior Front end Designer",
+      ubicacionEmpleo: "London, GB",
+      descripcionEmpleo: 'Who we are Yoti is a global company whose aim is to be the world rsquo s trusted identity system We have seriously ambitious goals to become the world s most secure and data sensitive company Our passionate team works to improve the way people share their personal information Innovative',
+      tipoEmpleo: 'Jornada completa'
+    },
+    {
+      imgEmpleo: 'https://media-exp1.licdn.com/dms/image/C4E0BAQFs5HVdyuXuww/company-logo_100_100/0?e=1594857600&v=beta&t=Ib-0DQg2eYvLjf6bEdS-w-U_YjoxAQehdtgtMJAHIeA',
+      nameEmpleo: "Analista o Auxiliar de Comercio Exterior Rionegro",
+      ubicacionEmpleo: "Medellín",
+      descripcionEmpleo: 'As a Software Engineer, you will have the opportunity to design and develop industry-leading commercial software, known for its innovation and usability. You will also be responsible for performing basic software testing tasks including, but not limited to, test development, planning, scheduling, and execution.',
+      tipoEmpleo: 'Jornada completa'
+    },
+    {
+      imgEmpleo: 'https://media-exp1.licdn.com/dms/image/C4D0BAQG-fp1l8bN-8w/company-logo_100_100/0?e=1594857600&v=beta&t=EeJgVJHzP-Dxv17kh_9BFBB2SUV7HfcnX3d--4F3fds',
+      nameEmpleo: "Software Engineer",
+      ubicacionEmpleo: "London, GB",
+      descripcionEmpleo: 'Who we are Yoti is a global company whose aim is to be the world rsquo s trusted identity system We have seriously ambitious goals to become the world s most secure and data sensitive company Our passionate team works to improve the way people share their personal information Innovative',
+      tipoEmpleo: 'Jornada completa'
+    },
+    {
+      imgEmpleo: 'https://media-exp1.licdn.com/dms/image/C560BAQFKZzUH4yMNZg/company-logo_100_100/0?e=1594857600&v=beta&t=_awAb5B7FOKcSULMTsT9TnKYVbqAEtThfA5mPQF69Q4',
+      nameEmpleo: "Junior Front end Designer",
+      ubicacionEmpleo: "London, GB",
+      descripcionEmpleo: 'Who we are Yoti is a global company whose aim is to be the world rsquo s trusted identity system We have seriously ambitious goals to become the world s most secure and data sensitive company Our passionate team works to improve the way people share their personal information Innovative',
+      tipoEmpleo: 'Jornada completa'
     }
   ],
 
