482c0779746bbc233cef48ffa8ab59315f10316b
Derly <derlyvivianamurcia@gmail.com>
2020-04-12 13:23:49 -0500

    Actualización cards

--- src/components/ItemsEmpleos.js
+++ src/components/ItemsEmpleos.js
@@ -20,13 +20,14 @@ function ItemsEmpleos(props) {
                         {ItemsEmpleos.map((item, index) => {
                             return (
                                 <div className="col-lg-3 col-md-6" key={`ItemsEmpleos-item-${index}`}>
-                                    <div className=" feature_item ">
-                                        <img src=''></img>
-                                        <h4>{item.nameEmpleo}</h4>
-                                        <p>{item.ubicacionEmpleo}</p>
-                                        <p><strong>Descripción:</strong>{item.descripcionEmpleo} </p>
-                                        <p><strong>Tipo empleo:</strong> {item.tipoEmpleo} </p>
-                                        <div className="col-lg-3 col-md-6">
+                                    <div class="card" key={`ItemsEmpleos-item-${index}`}>
+                                        <img src={item.imgEmpleo} class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">{item.nameEmpleo}</h5>
+                                            <p>{item.ubicacionEmpleo}</p>
+                                            <p class="card-text"><strong>Descripción: </strong>{item.descripcionEmpleo}</p>
+                                            <p><strong>Tipo empleo:</strong> {item.tipoEmpleo} </p>
+                                            <a href="#" class="btn btn-primary">VER EMPLEO</a>
                                         </div>
                                     </div>
                                 </div>
--- src/components/MainEmpleabilidad.js
+++ src/components/MainEmpleabilidad.js
@@ -21,7 +21,6 @@ class MainEmpleabilidad extends React.Component {
                                             <select id="inputState" class="form-control">
                                                 <option selected>Tipo de Contrato...</option>
                                                 <option>...</option>
-
                                             </select>
                                         </div>
                                         <div class="col">
@@ -35,9 +34,8 @@ class MainEmpleabilidad extends React.Component {
                                         </div>
                                         <div class="col">
                                         <button className="btn btn-lg btn-btn btn-info buttonForm text-uppercase" type="submit">Buscar empleo</button>
-                                        </div>                                      
+                                        </div>
                                     </div>
-
                                     <div className="container">
                                         <div className="row">
                                             <div class="col">
@@ -45,56 +43,15 @@ class MainEmpleabilidad extends React.Component {
                                                 <div class="col">
                                                     <button className="btn btn-lg btn-btn btn-info buttonForm text-uppercase" type="submit">Ingrese su hoja de vida</button>
                                                 </div>
-
                                             </div>
                                         </div>
-
-
-
                                         <ItemsEmpleos ItemsEmpleos={data.ItemsEmpleos} />
-
-                                        <section className="banner_area">
-                                            <div className="banner_inner d-flex align-items-center">
-                                                <div className="container">
-                                                    <div className="banner_content text-center">
-                                                        <div className="page_link">
-                                                           
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                        
-
-                                    </div>
-
-
-                                    <div class="form-row">
-                                        <div class="col">
-                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </>
         );
     }
