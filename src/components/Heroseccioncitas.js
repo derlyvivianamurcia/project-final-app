8ca6c11c599c1ec08f3e7e419f5c37df7c5ef20a
Luisa Villa <luisavilla@Luisas-MacBook-Pro.local>
2020-04-20 13:12:30 -0500

    se agrega envio de correo en opcion de acompañamiento

--- src/components/Heroseccioncitas.js
+++ src/components/Heroseccioncitas.js
@@ -5,6 +5,20 @@ import axios from "axios";
 import InicioSesion from "../pages/InicioSesion";
 
 class Heroseccioncitas extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      form: {
+        nombreUsuario: "",
+        email: "",
+        preguntas: "",
+      }
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   state = {
     loading: false,
     error: null,
@@ -46,14 +60,29 @@ class Heroseccioncitas extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const usuarioAutenticado = JSON.parse(localStorage.getItem("access_token"));
-    if (usuarioAutenticado) {
-      window.confirm("Su petición fue realizada con éxito, muy pronto nos comunicaremos contigo");
-     
-  }   else {
-    window.confirm("Para realizar la solicitud, debe iniciar sesión");
-   
-  }
+    if (usuarioAutenticado){
+      const templateId = 'template_bYLUDNF1';
+      this.sendFeedback(templateId, {message_html: this.state.form.preguntas, 
+                                    from_name: this.state.form.nombreUsuario,
+                                    reply_to: this.state.form.email})
+    } else {
+      alert("Por favor inicia sesion para realizar tu solicitud")
+    }
 };
+
+sendFeedback (templateId, variables) {
+  console.log(variables)
+	window.emailjs.send(
+  	'gmail', templateId,
+    variables
+  	).then(res => {
+      alert("Solicitud enviada correctamente")
+    	//console.log('Email successfully sent!')
+  	})
+  	// Handle errors here however you like, or use a React error boundary
+  	.catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+  }
+
   render() {
     return (
       <section className="container btext marginTop">
@@ -90,7 +119,7 @@ class Heroseccioncitas extends React.Component {
                 </li>
               </ol>
             </div>
-            <form onSubmit={this.handleSubmit}>
+            <form>
               <div className="form-group">
                 <label>Nombre</label>
                 <input
@@ -123,15 +152,9 @@ class Heroseccioncitas extends React.Component {
                   value={this.state.form.preguntas}
                 ></textarea>
                 </div>
+                
             <div className="form-group d-flex justify-content-center">
-              <button
-                type="submit"
-                className="btn btn-orange font-weight-bold"
-                onClick={this.handleSubmit}
-
-              >
-                Solicitar cita
-              </button>
+            <input type="button" value="Solicitar cita" className="btn btn-orange font-weight-bold" onClick={this.handleSubmit} />
               </div>
             </form>
           </div>
